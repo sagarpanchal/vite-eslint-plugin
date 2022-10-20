3ae9311965b5fbc02f61a8a4e8dddb421bcbd71e
Sagar Panchal <panchal.sagar@outlook.com>
2022-10-20 12:44:23 +0530

    use npm

--- playground/vue-ts/vite.config.ts
+++ playground/vue-ts/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import eslint from 'vite-plugin-eslint'
+import eslint from '../../dist'
 
 // https://vitejs.dev/config/
 export default defineConfig({
